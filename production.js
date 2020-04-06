300be1376aaa749e78973f95436c016ce363a2da
齐飘洋 <qipiaoyang@outlook.com>
2020-04-06 20:41:33 +0800

    更新

--- production.js
+++ production.js
@@ -1,10 +1,8 @@
-const path = require('path');
 const Application = require('thinkjs');
-
+const watcher = require('think-watcher');
 const instance = new Application({
   ROOT_PATH: __dirname,
-  APP_PATH: path.join(__dirname, 'app'),
-  // proxy: true, // use proxy
+  watcher: watcher,
   env: 'production'
 });
 
