59d81be6f1f9bb4dd40f127e3e8f09db593771e7
齐飘洋 <qipiaoyang@outlook.com>
2020-04-06 16:06:47 +0800

    更新路由表

--- src/common/config/router.js
+++ src/common/config/router.js
@@ -1,5 +1,6 @@
 module.exports = [
-  [/\/admin\/v1\/user(?:\/(\d+))?/, 'admin/v1/user?id=:1', 'rest']
+  [/\/admin\/v1\/user(?:\/(\d+))?/, 'admin/v1/user?id=:1', 'rest'],
+  [/\/admin\/v1\/auth_role(?:\/(\d+))?/, 'admin/v1/auth_role?id=:1', 'rest'],
 ];
 
 
